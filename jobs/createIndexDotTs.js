fc4c131cad3275fec1682d74a6e78d8bea00e767
Raj <raj.pulapakura@gmail.com>
2022-01-17 10:17:41 +1100

    testing

--- jobs/createIndexDotTs.js
+++ jobs/createIndexDotTs.js
@@ -2,36 +2,21 @@ const { writeFile } = require("fs").promises;
 const { resolve } = require("path");
 
 module.exports.createIndexDotTs = async (dir, session) => {
-  await writeFile(
-    resolve(dir, "src", "index.ts"),
-    `import "reflect-metadata";
+  if (session) {
+    await writeFile(
+      resolve(dir, "src", "index.ts"),
+      `import "reflect-metadata";
 import express from "express";
-${
-  session
-    ? `
 import session from "express-session";
-import connectRedis from "connect-redis";`
-    : ""
-}
+import connectRedis from "connect-redis";
 import cors from "cors";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { config } from "dotenv";
 
-${!session ? `import { env } from "./constants";` : ""}
 import { connectToDB } from "./utils/connectToDB";
-${
-  session
-    ? `
-import { connectToRedis } from "./utils/connectToRedis";`
-    : ""
-}
-${
-  session
-    ? `
-import { env, AUTH_COOKIE, SECRET } from "./constants";`
-    : ""
-}
+import { connectToRedis } from "./utils/connectToRedis";
+import { env, AUTH_COOKIE, SECRET } from "./constants";
 import { BookResolver } from "./features/Book/BookResolver";
 import { AuthorResolver } from "./features/Author/AuthorResolver";
 
@@ -44,9 +29,7 @@ const main = async () => {
   // Connect to database
   await connectToDB();
 
-  ${
-    session
-      ? `  const RedisStore = connectRedis(session);
+ const RedisStore = connectRedis(session);
   const redisClient = await connectToRedis();
 
   app.use(
@@ -58,9 +41,7 @@ const main = async () => {
       secret: SECRET,
       name: AUTH_COOKIE,
     })
-  );`
-      : ""
-  }
+  );
 
   app.use(cors({
     credentials: true,
@@ -88,6 +69,59 @@ const main = async () => {
 
 main().catch((e) => console.error(e));
 `,
-    { encoding: "utf-8" }
-  );
+      { encoding: "utf-8" }
+    );
+  } else {
+    await writeFile(
+      resolve(dir, "src", "index.ts"),
+      `import "reflect-metadata";
+import express from "express";
+import cors from "cors";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { config } from "dotenv";
+
+import { env } from "./constants";
+import { connectToDB } from "./utils/connectToDB";
+import { BookResolver } from "./features/Book/BookResolver";
+import { AuthorResolver } from "./features/Author/AuthorResolver";
+
+const main = async () => {
+  const app = express();
+
+  // Fetch environment variables
+  config();
+
+  // Connect to database
+  await connectToDB();
+
+  app.use(cors({
+    credentials: true,
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Set-Cookie"],
+    origin: ["https://studio.apollographql.com"],
+  }));
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      validate: false,
+      resolvers: [BookResolver, AuthorResolver],
+    }),
+    context: ({ req, res }) => ({ req, res, redis }),
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app, cors: false });
+
+  app.listen(env.PORT, () => {
+    console.log(\`The server is listening on port \${env.PORT}\`);
+  });
+};
+
+main().catch((e) => console.error(e));
+`,
+      { encoding: "utf-8" }
+    );
+  }
 };
